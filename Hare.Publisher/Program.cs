e895d714d37499d5e9ae30fa99266f10131fa4d7
inathi.gqola <inathi.gqola@gmail.com>
2018-05-10 22:23:19 +0200

    Add project files.

---
+++ Hare.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hare.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
